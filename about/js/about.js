27cb2ddb4ad991067895319b061b708577f9ecbf
ldywayne <2010477739@qq.com>
2022-06-08 14:02:24 +0800

    first commit

---
+++ about/js/about.js
@@ -0,0 +1,75 @@
+var sidebar1=document.getElementById('sidebar1');
+        sidebar1.onclick=sidebarOnclick1;
+        function sidebarOnclick1()
+        {
+            var tit1=document.getElementById('tit1');tit1.style.display='grid';
+            var sub1=document.getElementById('sub1');sub1.style.display='grid';
+            var tit2=document.getElementById('tit2');tit2.style.display='none';
+            var sub2=document.getElementById('sub2');sub2.style.display='none';
+            var tit3=document.getElementById('tit3');tit3.style.display='none';
+            var sub3=document.getElementById('sub3');sub3.style.display='none';
+            var tit4=document.getElementById('tit4');tit4.style.display='none';
+            var sub4=document.getElementById('sub4');sub4.style.display='none';
+            var tit5=document.getElementById('tit5');tit5.style.display='none';
+            var sub5=document.getElementById('sub5');sub5.style.display='none';
+        }
+        var sidebar2=document.getElementById('sidebar2');
+        sidebar2.onclick=sidebarOnclick2;
+        function sidebarOnclick2()
+        {
+            var tit1=document.getElementById('tit1');tit1.style.display='none';
+            var sub1=document.getElementById('sub1');sub1.style.display='none';
+            var tit2=document.getElementById('tit2');tit2.style.display='grid';
+            var sub2=document.getElementById('sub2');sub2.style.display='grid';
+            var tit3=document.getElementById('tit3');tit3.style.display='none';
+            var sub3=document.getElementById('sub3');sub3.style.display='none';
+            var tit4=document.getElementById('tit4');tit4.style.display='none';
+            var sub4=document.getElementById('sub4');sub4.style.display='none';
+            var tit5=document.getElementById('tit5');tit5.style.display='none';
+            var sub5=document.getElementById('sub5');sub5.style.display='none';
+        }
+        var sidebar3=document.getElementById('sidebar3');
+        sidebar3.onclick=sidebarOnclick3;
+        function sidebarOnclick3()
+        {
+            var tit1=document.getElementById('tit1');tit1.style.display='none';
+            var sub1=document.getElementById('sub1');sub1.style.display='none';
+            var tit2=document.getElementById('tit2');tit2.style.display='none';
+            var sub2=document.getElementById('sub2');sub2.style.display='none';
+            var tit3=document.getElementById('tit3');tit3.style.display='grid';
+            var sub3=document.getElementById('sub3');sub3.style.display='grid';
+            var tit4=document.getElementById('tit4');tit4.style.display='none';
+            var sub4=document.getElementById('sub4');sub4.style.display='none';
+            var tit5=document.getElementById('tit5');tit5.style.display='none';
+            var sub5=document.getElementById('sub5');sub5.style.display='none';
+        }
+        var sidebar4=document.getElementById('sidebar4');
+        sidebar4.onclick=sidebarOnclick4;
+        function sidebarOnclick4()
+        {
+            var tit1=document.getElementById('tit1');tit1.style.display='none';
+            var sub1=document.getElementById('sub1');sub1.style.display='none';
+            var tit2=document.getElementById('tit2');tit2.style.display='none';
+            var sub2=document.getElementById('sub2');sub2.style.display='none';
+            var tit3=document.getElementById('tit3');tit3.style.display='none';
+            var sub3=document.getElementById('sub3');sub3.style.display='none';
+            var tit4=document.getElementById('tit4');tit4.style.display='grid';
+            var sub4=document.getElementById('sub4');sub4.style.display='grid';
+            var tit5=document.getElementById('tit5');tit5.style.display='none';
+            var sub5=document.getElementById('sub5');sub5.style.display='none';
+        }
+        var sidebar5=document.getElementById('sidebar5');
+        sidebar5.onclick=sidebarOnclick5;
+        function sidebarOnclick5()
+        {
+            var tit1=document.getElementById('tit1');tit1.style.display='none';
+            var sub1=document.getElementById('sub1');sub1.style.display='none';
+            var tit2=document.getElementById('tit2');tit2.style.display='none';
+            var sub2=document.getElementById('sub2');sub2.style.display='none';
+            var tit3=document.getElementById('tit3');tit3.style.display='none';
+            var sub3=document.getElementById('sub3');sub3.style.display='none';
+            var tit4=document.getElementById('tit4');tit4.style.display='none';
+            var sub4=document.getElementById('sub4');sub4.style.display='none';
+            var tit5=document.getElementById('tit5');tit5.style.display='grid';
+            var sub5=document.getElementById('sub5');sub5.style.display='grid';
+        }
